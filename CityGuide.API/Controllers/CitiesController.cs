445ed65fa847bce70991ca58c1de01950d226e1f
Ashley Saylik <ashley.saylik@tetratech.com>
2019-08-20 15:25:54 -0500

    Added Cities Controller and AutoMapper requirement

---
+++ CityGuide.API/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityGuide.API.Data;
+using CityGuide.API.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityGuide.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private IAppRepository _appRepository;       
+
+        public CitiesController(IAppRepository appRepository)
+        {
+            _appRepository = appRepository;       
+        }
+
+        public ActionResult GetCities()
+        {
+            var cities = _appRepository.GetCities();
+            var citiesToReturn = _mapper.Map<List<CityForListDto>>(cities);
+            return Ok(citiesToReturn);
+        }
+    }
+}
